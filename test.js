f3a73c9722e2f84b3205416f19a09ed4f885d60d
MohamadAli <mmdalix@gmail.com>
2019-04-26 16:00:34 +0430

    A

---
+++ test.js
@@ -0,0 +1,3 @@
+let report_server_app = "http://86.104.32.254/app"
+var EventTransitter = require("./index.js")("mokhche",report_server_app,"admin","fvkvHqZh8B56egk8");
+EventTransitter.postEvent("susbsrcibe",{pn:"912"});
